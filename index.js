48a90c63116091461285986513f66ef30b0a228f
Nathan McKenzie <nathanmckenzie@Nathans-MacBook-Air.local>
2020-11-08 08:43:13 -0800

    cryptocurrency app

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
+  }
+}
+
+
+
+class Withdrawal extends Transaction {
+
+  get Value() {
+    return this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get Value() {
+    return -this.amount;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("nate")
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+
+t3 = new Deposit(1120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
 
 console.log('Balance:', balance);
